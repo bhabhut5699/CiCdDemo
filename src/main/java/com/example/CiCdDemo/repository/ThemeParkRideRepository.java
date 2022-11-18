aa1b6f22693bdb3dc4e1d8c95c476a56e26f5ad4
bhabhut5699 <chaudharib796@gmail.com>
2022-11-18 15:56:34 +0530

    first commit

---
+++ src/main/java/com/example/CiCdDemo/repository/ThemeParkRideRepository.java
@@ -0,0 +1,10 @@
+package com.example.CiCdDemo.repository;
+
+import com.example.CiCdDemo.entity.ThemeParkRide;;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
